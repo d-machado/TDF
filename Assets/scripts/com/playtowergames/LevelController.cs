4f0c70ada4c1b61d8eef336cdfea645ded9bd823
Diego Machado <tuxvader@gmail.com>
2014-05-05 22:45:47 -0300

    Creating flag

--- Assets/scripts/com/playtowergames/LevelController.cs
+++ Assets/scripts/com/playtowergames/LevelController.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class LevelController : MonoBehaviour {
 
 	public GameObject flag;
+
+	private GameObject aFlag;
+
 	// Use this for initialization
 	void Start () {
-	
+		aFlag = Instantiate (flag) as GameObject;
+		iTween.PutOnPath (aFlag, iTweenPath.GetPath ("enemy_path_1"), .95f);
 	}
 	
 	// Update is called once per frame
