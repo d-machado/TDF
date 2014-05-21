b9c9154516ecc81725ef993adf32ec179ebf72b8
Diego Machado <tuxvader@gmail.com>
2014-05-20 21:57:28 -0300

    Added NGUI

---
+++ Assets/scripts/com/playtowergames/GUI/PreloaderGUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreloaderGUI : MonoBehaviour {
+
+    public string PlayerURL = "http://playtowergames.com/custom_games/tdf/current/tdf.unity3d";
+    public UILabel LoadingText;
+
+    private WWW stream;
+    void Awake()
+    {
+        
+    }
+
+	void Start () {
+
+        stream = new WWW(PlayerURL);
+        StartCoroutine(LoadingProgress());
+	}
+
+    private IEnumerator LoadingProgress()
+    {
+        while (!stream.isDone)
+        {
+            Debug.Log("Loading");
+            LoadingText.text = "LOADING " + (int)(stream.progress * 100) + "%";
+            yield return new WaitForEndOfFrame();
+        }
+        stream.LoadUnityWeb();
+    }
+	
+	
+}
